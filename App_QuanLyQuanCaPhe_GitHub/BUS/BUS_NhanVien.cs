5a8ae1ea2f59def12c9157b3441918a74d25c8b0
TienDung2908 <37709473+TienDung2908@users.noreply.github.com>
2019-03-08 14:33:21 +0700

    add Bus_NhanVien

---
+++ App_QuanLyQuanCaPhe_GitHub/BUS/BUS_NhanVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAO;
+using DTO;
+using System.Data;
+namespace BUS
+{
+    public class BUS_NhanVien
+    {
+        DAO_KetNoi xl = new DAO_KetNoi();
+        DTO_NhanVien dl = new DTO_NhanVien();
+    }
+}
