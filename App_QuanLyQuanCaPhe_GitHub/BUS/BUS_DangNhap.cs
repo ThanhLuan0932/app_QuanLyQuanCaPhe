8d11500b5a4b0f6f5f6620d49d988e1bbb7f8bc7
cuongdo1997 <chicuong0997@gmail.com>
2019-03-14 13:55:10 +0700

    DangNhap_Select

--- App_QuanLyQuanCaPhe_GitHub/BUS/BUS_DangNhap.cs
+++ App_QuanLyQuanCaPhe_GitHub/BUS/BUS_DangNhap.cs
@@ -10,6 +10,10 @@ namespace BUS
     public class BUS_DangNhap
     {
         DAO_KetNoi xl = new DAO_KetNoi();
-        DTO_DangNhap dl = new DTO_DangNhap();        
+        DTO_DangNhap dl = new DTO_DangNhap();
+        public DataTable DangNhap_Select(DTO_DangNhap dl)
+        {
+            return xl.table_Select("select * from MayChu");
+        }
     }
 }
