fe145ee2747bfb27277e336497d63dbf36075a52
ngochung8546 <ngochung8546@gmail.com>
2019-03-15 13:09:47 +0700

    add uc_QuanLy_LoaiSanPham.Designer.cs

--- App_QuanLyQuanCaPhe_GitHub/GUI/ucQuanLy_LoaiSanPham.Designer.cs
+++ App_QuanLyQuanCaPhe_GitHub/GUI/ucQuanLy_LoaiSanPham.Designer.cs
@@ -28,10 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtChonLoai = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtgMenu = new System.Windows.Forms.DataGridView();
+            this.MaLoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TrangThai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenLoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMaLoai = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btPhucHoi = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btHuy = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgMenu)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtChonLoai);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dtgMenu);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(78, -5);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(604, 376);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu";
+            // 
+            // txtChonLoai
+            // 
+            this.txtChonLoai.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChonLoai.Location = new System.Drawing.Point(313, 20);
+            this.txtChonLoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtChonLoai.Name = "txtChonLoai";
+            this.txtChonLoai.Size = new System.Drawing.Size(173, 26);
+            this.txtChonLoai.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(185, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Loại Sản Phẩm";
+            // 
+            // dtgMenu
+            // 
+            this.dtgMenu.AllowUserToAddRows = false;
+            this.dtgMenu.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgMenu.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgMenu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgMenu.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dtgMenu.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dtgMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgMenu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaLoai,
+            this.TrangThai,
+            this.TenLoai});
+            this.dtgMenu.Location = new System.Drawing.Point(10, 50);
+            this.dtgMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgMenu.MultiSelect = false;
+            this.dtgMenu.Name = "dtgMenu";
+            this.dtgMenu.ReadOnly = true;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgMenu.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dtgMenu.RowTemplate.Height = 24;
+            this.dtgMenu.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgMenu.Size = new System.Drawing.Size(584, 318);
+            this.dtgMenu.TabIndex = 1;
+            // 
+            // MaLoai
+            // 
+            this.MaLoai.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.MaLoai.DataPropertyName = "MaLoai";
+            this.MaLoai.HeaderText = "Mã Loại";
+            this.MaLoai.Name = "MaLoai";
+            this.MaLoai.ReadOnly = true;
+            this.MaLoai.Width = 80;
+            // 
+            // TrangThai
+            // 
+            this.TrangThai.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.TrangThai.DataPropertyName = "TrangThai";
+            this.TrangThai.HeaderText = "Trạng Thái";
+            this.TrangThai.Name = "TrangThai";
+            this.TrangThai.ReadOnly = true;
+            this.TrangThai.Width = 200;
+            // 
+            // TenLoai
+            // 
+            this.TenLoai.DataPropertyName = "TenLoai";
+            this.TenLoai.HeaderText = "Tên Loại Sản Phẩm";
+            this.TenLoai.Name = "TenLoai";
+            this.TenLoai.ReadOnly = true;
+            // 
+            // txtMaLoai
+            // 
+            this.txtMaLoai.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaLoai.Location = new System.Drawing.Point(15, 110);
+            this.txtMaLoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMaLoai.Name = "txtMaLoai";
+            this.txtMaLoai.Size = new System.Drawing.Size(59, 26);
+            this.txtMaLoai.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Mã Loại";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btPhucHoi);
+            this.groupBox2.Controls.Add(this.btXoa);
+            this.groupBox2.Controls.Add(this.btHuy);
+            this.groupBox2.Controls.Add(this.btLuu);
+            this.groupBox2.Controls.Add(this.txtMaLoai);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btThem);
+            this.groupBox2.Location = new System.Drawing.Point(688, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(225, 368);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức Năng";
+            // 
+            // btPhucHoi
+            // 
+            this.btPhucHoi.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btPhucHoi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btPhucHoi.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPhucHoi.Location = new System.Drawing.Point(0, 275);
+            this.btPhucHoi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btPhucHoi.Name = "btPhucHoi";
+            this.btPhucHoi.Size = new System.Drawing.Size(117, 92);
+            this.btPhucHoi.TabIndex = 6;
+            this.btPhucHoi.Text = "Phục Hồi Sản Phẩm";
+            this.btPhucHoi.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btPhucHoi.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btXoa.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(0, 78);
+            this.btXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(117, 92);
+            this.btXoa.TabIndex = 5;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // btHuy
+            // 
+            this.btHuy.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btHuy.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btHuy.Location = new System.Drawing.Point(0, 17);
+            this.btHuy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btHuy.Name = "btHuy";
+            this.btHuy.Size = new System.Drawing.Size(222, 56);
+            this.btHuy.TabIndex = 3;
+            this.btHuy.Text = "Tải Lại";
+            this.btHuy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btHuy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btHuy.UseVisualStyleBackColor = true;
+            // 
+            // btLuu
+            // 
+            this.btLuu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLuu.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLuu.Location = new System.Drawing.Point(122, 78);
+            this.btLuu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(91, 289);
+            this.btLuu.TabIndex = 3;
+            this.btLuu.Text = "Lưu";
+            this.btLuu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btLuu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btLuu.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btThem.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btThem.Location = new System.Drawing.Point(0, 175);
+            this.btThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(117, 96);
+            this.btThem.TabIndex = 2;
+            this.btThem.Text = "Thêm ";
+            this.btThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btThem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // ucQuanLy_LoaiSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "ucQuanLy_LoaiSanPham";
+            this.Size = new System.Drawing.Size(990, 367);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgMenu)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtChonLoai;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dtgMenu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaLoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TrangThai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenLoai;
+        private System.Windows.Forms.TextBox txtMaLoai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btPhucHoi;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btHuy;
+        private System.Windows.Forms.Button btLuu;
+        private System.Windows.Forms.Button btThem;
     }
 }
--- App_QuanLyQuanCaPhe_GitHub/GUI/ucQuanLy_LoaiSanPham.cs
+++ App_QuanLyQuanCaPhe_GitHub/GUI/ucQuanLy_LoaiSanPham.cs
@@ -5,8 +5,11 @@ using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BUS;
+using DAO;
+using DTO;
 namespace GUI
 {
     public partial class ucQuanLy_LoaiSanPham : UserControl
@@ -15,5 +18,60 @@ namespace GUI
         {
             InitializeComponent();
         }
+        BUS_QuanLy xldl = new BUS_QuanLy();
+        DTO_QuanLy dl = new DTO_QuanLy();
+        void Binding()
+        {
+            txtChonLoai.DataBindings.Clear();
+            txtChonLoai.DataBindings.Add("Text", dtgMenu.DataSource, "TenLoai");
+            txtMaLoai.DataBindings.Clear();
+            txtMaLoai.DataBindings.Add("Text", dtgMenu.DataSource, "MaLoai");
+        }
+        void Clear()
+        {
+            txtChonLoai.ResetText();
+            txtMaLoai.Clear();
+        }
+        private void Enable_False()
+        {
+            txtChonLoai.Enabled = false;
+        }
+        private void Enable_True()
+        {
+            txtChonLoai.Enabled = true;
+        }
+        private void btHuy_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btPhucHoi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ucQuanLy_LoaiSanPham_Load(object sender, EventArgs e)
+        {
+            dtgMenu.DataSource = xldl.ChonLoai_Select(dl);
+            Enable_False();
+        }
+        private void txtChonLoai_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        bool them = false;
     }
 }
