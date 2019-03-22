6937169ef4c2f7e30060b413fdc5ab43d521716b
Luan <33895995+ThanhLuan0932@users.noreply.github.com>
2019-03-22 13:37:27 +0700

    fix_Hinh

--- App_QuanLyQuanCaPhe_GitHub/GUI/Properties/Resources.Designer.cs
+++ App_QuanLyQuanCaPhe_GitHub/GUI/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace GUI.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,26 +57,6 @@ namespace GUI.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo_1 {
-            get {
-                object obj = ResourceManager.GetObject("logo_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap power_button_off {
-            get {
-                object obj = ResourceManager.GetObject("power-button-off", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
